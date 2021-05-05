535563c4c5b0e0886f1ecfa97f73217786eb4d55
leaderruvuna <ruvunaraymond@gmail.com>
2021-05-05 12:27:42 +0300

    Init modules

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace modules_n_csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         //INIT WORK
+            
+        }
+
+    }
+}     
